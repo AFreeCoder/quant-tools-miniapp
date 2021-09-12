1562179c8ae6b8473d99add1f0d19cb8ae164836
wanghaijie01 <wanghaijie01@baidu.com>
2021-09-13 00:57:17 +0800

    optimize

---
+++ pages/etf/etf.vue
@@ -0,0 +1,39 @@
+<template>
+	<view>
+		<official-account></official-account>
+		<view>
+			<qiun-title-bar title="ETF候选集—估值"/>
+			<text style="margin-left: 32rpx;">沪深300</text></br>
+			<text style="margin-left: 32rpx;">中证500</text></br>
+			<text style="margin-left: 32rpx;">上证50</text></br>
+			<text style="margin-left: 32rpx;">全指信息</text></br>
+			<text style="margin-left: 32rpx;">中证养老</text></br>
+			<text style="margin-left: 32rpx;">中证消费</text></br>
+			<text style="margin-left: 32rpx;">全指医药</text></br>
+			<text style="margin-left: 32rpx;">创业板指</text></br>
+			<text style="margin-left: 32rpx;">中证红利</text>
+		</view>
+		<view>
+			<qiun-title-bar title="ETF相关性"/>
+			<text style="margin-left: 24rpx;">待展示</text></br>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- pages/market/market.vue
+++ pages/market/market.vue
@@ -1,15 +1,18 @@
 <template>
 	<view class="content">
 		<official-account></official-account>
-		<qiun-title-bar title="集思录温度计"/>
-		<!-- 这里的title.name和subtitle.name如果需要联动chartData，请定义一个变量同步传入:opts和:chartData中 -->
-		<view class="charts-box">
-		  <qiun-data-charts type="gauge" :opts="{title:{name: temperature,color: '#2fc25b',fontSize: 25,offsetY:50},subtitle: {name: '当前温度',color: '#666666',fontSize: 15,offsetY:-50}}" :chartData="chartsDataGauge1"/>
+		<view style="margin-bottom: 15rpx;">
+			<qiun-title-bar title="市场温度计"/>
+			<!-- 这里的title.name和subtitle.name如果需要联动chartData，请定义一个变量同步传入:opts和:chartData中 -->
+			<view class="charts-box" style="vertical-align: middle;">
+				<qiun-data-charts type="gauge" :opts="{title:{name: temperature,color: '#2fc25b',fontSize: 25,offsetY:50},subtitle: {name: '当前温度',color: '#666666',fontSize: 15,offsetY:-50}}" :chartData="chartsDataGauge1"/>
+			</view>
+			<text style="margin-left: 25rpx;color: #007AFF;" @click="setClipboard">注：点击查看温度计详情。</text>
 		</view>
-		<qiun-title-bar title="蛋卷估值"/>
 		<!-- 这里的title.name和subtitle.name如果需要联动chartData，请定义一个变量同步传入:opts和:chartData中 -->
-		<!-- <view class="charts-box"> -->
-			<uni-table border stripe emptyText="暂无更多数据" >
+		<view>
+			<qiun-title-bar title="指数估值"/>
+			<uni-table border stripe emptyText="暂无更多数据">
 			    <!-- 表头行 -->
 			    <uni-tr>
 			        <uni-th align="center" width="100">指数名称</uni-th>
@@ -35,8 +38,7 @@
 					<uni-td>{{item.peg}}</uni-td>
 			    </uni-tr>
 			</uni-table>
-			</script>
-		<!-- </view> -->
+		</view>
 	</view>
 </template>
 
@@ -64,6 +66,15 @@
 		onLoad() {
 		  //从服务器获取数据
 		  this.getServerData()
+		},
+		onShareAppMessage(res) {
+		    if (res.from === 'button') {// 来自页面内分享按钮
+		      console.log(res.target)
+		    }
+		    return {
+		      title: '投资工具箱——市场温度计',
+		      path: '/pages/market/market'
+		    }
 		},
 		methods: {
 			getServerData() {
@@ -101,6 +112,27 @@
 					method: 'GET',
 					success: (res) => {
 						this.index_eva = res.data.data.items;
+					}
+				})
+			},
+			setClipboard: function () {
+				uni.setClipboardData({
+					data: "https://www.jisilu.cn/data/indicator",
+					success: () => {
+						// 成功处理
+						uni.showToast({
+							title: '复制成功，请到浏览器查看',
+							icon: "none",
+							mask: !1
+						})
+					},
+					fail: () => {
+						// 失败处理
+						uni.showToast({
+							title: '复制失败',
+							icon: "none",
+							mask: !1
+						})
 					}
 				})
 			}
